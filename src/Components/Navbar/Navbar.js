47787ed7b78bd92ee0fd2de3d77cb73540e87330
yangzhengca <yangzhengca>
2021-12-14 16:37:25 -0600

    fixed navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,45 +5,57 @@ import logo from "../../images/clinic.png";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-md navbar-dark sticky-top bg-primary ">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-          <img class={styles.logo} src={logo} />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ms-auto fs-3">
-            <Link smooth  to="#top" class="nav-link active" aria-current="page">
-              Home
-            </Link>
-            <Link smooth class="nav-link" to="#about">
-              About
-            </Link>
-            <Link smooth class="nav-link" to="#service">
-              Service
-            </Link>
-            <Link smooth class="nav-link" to="#contact">
-              Contact
-            </Link>
-            <div class="d-grid gap-2 col-3 mx-auto">
-              <a class="btn btn-small btn-success fs-5 text-wrap" href="#">
-                Make An Appointment
-              </a>
-            </div>
+    <>
+      <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+          {/* <a class="navbar-brand" href="/">
+            <img style={{maxWidth: "10%"}} src={logo} alt="logo" />
+          </a> */}
+          <a class="navbar-brand fs-2" href="#">The Clinic</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+              <li class="nav-item">
+                <a href="#top" class="nav-link" aria-current="page">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#about" class="nav-link">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#service" class="nav-link">
+                  Service
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="#contact" class="nav-link">
+                  Contact
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">
+                  Appointment
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
